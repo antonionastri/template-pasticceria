47edc06388c5ccaed19e005ab67da50f4b7343a3
antonionastri <nastritoni98@gmail.com>
2024-08-30 12:12:10 +0200

    1.5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ContattiComponent } from './components/contatti/contatti.component';
 
 export const routes: Routes = [
     {
         path:"", component:HomeComponent
+    },
+    {
+        path:"contatti", component:ContattiComponent    
     }
 ];
